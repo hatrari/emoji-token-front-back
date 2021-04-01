021efcd96bef3e32991fa0805308689ce190e2e9
hatrari <ham.atrari@gmail.com>
2021-04-01 15:49:51 +0200

    add rinkeby network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,5 +9,11 @@ module.exports = {
         mnemonic: "record excuse try include danger agree much rapid globe drastic profit edit"
       }
     },
+    rinkeby: {
+			url: "https://rinkeby.infura.io/v3/93c484632ab74f578b0ab61b1a0e146c",
+			accounts: {
+				mnemonic: "record excuse try include danger agree much rapid globe drastic profit edit"
+			}
+		}
   }
 };
